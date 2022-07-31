bb6ebb79b0164fdf7567454ea299e2893b2702be
Gean Ferreira <gean_roberto@hotmail.com>
2022-07-31 14:06:29 -0300

    Importando os components na aplicacao

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import { Banner } from './components/Banner/index.js';
+import { Footer } from './components/Footer/index.js';
+import { Middle } from './components/Middle/index.js';
 import { Navbar } from './components/Navbar/index.js'
+import { Products } from './components/ProductsSection/index.js';
+import { Title } from './components/Title/index.js';
 
 function App() {
   return (
     <>
       <Navbar />
       <Banner />
+      <Middle />
+      <Products />
+      <Title />
+      <Footer />
     </>
   );
 }
