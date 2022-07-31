5554f6fdfc8b6471c0fb0102835b69a12415c453
Gean Ferreira <gean_roberto@hotmail.com>
2022-07-31 13:11:31 -0300

    FEAT: Implementando o component Footer e sua folha de estilo

---
+++ src/components/Footer/index.js
@@ -0,0 +1,9 @@
+import './style.css';
+
+export const Footer = () => {
+    return (
+        <footer id="footer">
+            <div class="footer__area container">Desenvolvido por gean_ferreira &#9830;</div>
+        </footer>
+    )
+}
