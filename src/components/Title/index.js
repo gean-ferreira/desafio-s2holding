940ac9f270645eb5271255c3b5b120fb8b7fdec6
Gean Ferreira <gean_roberto@hotmail.com>
2022-07-31 13:10:07 -0300

    FEAT: Implementando o component Title

---
+++ src/components/Title/index.js
@@ -0,0 +1,12 @@
+import './style.css';
+
+export const Title = () => {
+    return (
+        <section id="title" class="apresentation__area container space">
+            <h2 class="apresentation__title">O melhor site do Brasil!</h2>
+            <p class="description__title">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, inventore maiores
+                laboriosam dolor sapiente modi sint nostrum accusantium quaerat, eum deleniti officia sequi ducimus optio a rem,
+                amet labore animi? Sed ultrices molestie velit, a rutrum sapien fermentum quis. Curabitur pulvinar rhoncus faucibus. Sed eget lacus blandit, varius ex eu, elementum lacus. Aliquam porta viverra est, sit amet imperdiet ipsum auctor ac. Maecenas pellentesque ante id tortor iaculis, in suscipit tellus blandit. Duis pulvinar leo nec cursus congue. Aenean sollicitudin, mauris eu dapibus egestas, ex sem fermentum ligula, ac tempor eros erat eu quam. Aliquam eget dolor tellus. Maecenas quis dui id ipsum viverra placerat vel eu justo. Curabitur non purus sed nisl tincidunt rhoncus. Donec eu tortor ultricies, rutrum magna ac, porttitor augue. Nulla facilisi. Nam facilisis nisi id lorem lacinia rhoncus. Etiam tempus ligula ut nisl faucibus semper. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. </p>
+        </section>
+    )
+}
