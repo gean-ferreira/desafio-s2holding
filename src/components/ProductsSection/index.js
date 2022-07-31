17683aeece4674b132434fea2edcb125072437b2
Gean Ferreira <gean_roberto@hotmail.com>
2022-07-31 12:33:10 -0300

    FEAT: Sessao dos produtos
    Nesta sessao renderizara os produtos que serao consumido pela API fornecida no documento do desafio

---
+++ src/components/ProductsSection/index.js
@@ -0,0 +1,14 @@
+import './style.css';
+
+export const Products = () => {
+    return (
+        <main id="products" className="products__area container space">
+            <ul className="products__menu-list">
+                <li>produto</li>
+                <li>produto</li>
+                <li>produto</li>
+                <li>produto</li>
+            </ul>
+        </main>
+    )
+}
