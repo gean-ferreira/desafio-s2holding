c03f418d115ca0dd622b7ef2ac7d9b921521ce18
Gean Ferreira <gean_roberto@hotmail.com>
2022-07-31 12:04:05 -0300

    REFACTOR: Mudando comportamento do component Banner
    Mudando a logica de mudar as fotos por meio da media querie. Seguindo o que foi pedido no desafio

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,16 +1,10 @@
-import mobImg from '../../assets/imgs/banners/banner-full-mobile.jpg'
-import deskImg from '../../assets/imgs/banners/banner-full-desktop.jpg'
-import { isMobile } from '../../assets/scripts/functions';
+import './style.css';
 
 export const Banner = () => {
-
-    // Coloca a imagem de acordo com o viewport do usuário
-    const img = isMobile() ? mobImg : deskImg;
-
     return (
         <section id="banner">
             <a href="https://cantao.com.br" target="_self" rel="noopener noreferrer">
-                <img src={img} width="100%" height="auto" alt="Banner do site" />
+                <img width="100%" height='auto'  alt="Banner do site" />
             </a>
         </section>
     )
