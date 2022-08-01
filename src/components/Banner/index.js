ca488e66fda9637b8235157ff925c16e66793994
Gean Ferreira <gean_roberto@hotmail.com>
2022-07-31 23:23:30 -0300

    REFACTOR: Removendo a tag imagem e colocando o background na section

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -2,10 +2,9 @@ import './style.css';
 
 export const Banner = () => {
     return (
-        <section id="banner">
-            <a href="https://cantao.com.br" target="_self" rel="noopener noreferrer">
-                <img width="100%" height='auto'  alt="Banner do site" />
-            </a>
-        </section>
+        <a href="https://cantao.com.br" target="_self" rel="noopener noreferrer">
+            <section id="banner">
+            </section>
+        </a>
     )
 }
