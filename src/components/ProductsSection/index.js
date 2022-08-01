b5ab63720e6c400bc9bdb53e44b81db46dc26255
Gean Ferreira <gean_roberto@hotmail.com>
2022-07-31 23:48:52 -0300

    CHORE[17683ae]: Implementando funcao que ira consumir API fornecida no desafio
    O consumo deu erro de cors

--- src/components/ProductsSection/index.js
+++ src/components/ProductsSection/index.js
@@ -1,6 +1,36 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios'
 import './style.css';
 
 export const Products = () => {
+
+    const [products, setProducts] = useState([])
+    // const urlProducts = 'https://cantao.vtexcommercestable.com.br/api/catalog_system/pub/products/search?fC:65&_from=1&_to=50'
+    const urlProducts = "https://jsonplaceholder.typicode.com/users/1/albums"
+
+    const getAnswer = async () => {
+        
+        //Com axios
+        const response = await axios({
+            url: urlProducts, method: 'GET'
+            , headers: {
+                "content-type": "application/json; charset=utf-8",
+                'Access-Control-Allow-Origin': 'no-cors'
+            }
+        })
+        console.log(response)
+
+        // Com fetch
+        // const resp = await fetch(urlProducts)
+        // const json = await resp.json()
+        // console.log(json)
+    }
+
+    useEffect(() => {
+        getAnswer()
+    }, [])
+
+
     return (
         <main id="products" className="products__area container space">
             <h1 className='title'>Produtos</h1>
