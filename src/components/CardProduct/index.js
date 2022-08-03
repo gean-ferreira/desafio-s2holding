acb101b4a76f775e392997fbd46bef1638eb20b5
Gean Ferreira <gean_roberto@hotmail.com>
2022-08-02 22:42:40 -0300

    FEAT: Importando a funcao de conversao e deixando os cards alinhados ao meio do container pai (li)
    Neste commit tambem foi acrescentado media querie para aumentar a foto quando a tela for maior que 1200px

--- src/components/CardProduct/index.js
+++ src/components/CardProduct/index.js
@@ -1,23 +1,14 @@
 import './style.css';
+import { convertToReal } from '../../assets/scripts/functions';
 
 export const CardProduct = ({ link, image, name, price }) => {
   return (
-    <a
-      className='link__card-product'
-      href={link}
-      target='_self'
-      rel='noopener noreferrer'
-    >
-      <div className='product__container'>
-        <img
-          src={image}
-          alt={`Fotos de ${name}`}
-          width={200}
-          height={300}
-        />
+    <div className='product__container'>
+      <a className='link__product' href={link} target='_self' rel='noopener noreferrer'>
+        <img src={image} alt={`Fotos de ${name}`} width={'100%'} height={'auto'} />
         <h5 className='title__product'>{name}</h5>
-        <p className='price__product'>{price}</p>
-      </div>
-    </a>
+        <p className='price__product'>{convertToReal(price)}</p>
+      </a>
+    </div>
   );
 };
