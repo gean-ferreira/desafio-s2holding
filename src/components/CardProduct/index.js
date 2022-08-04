716ab0cfc878ec9e6dcd0ed3c162f8b5f390e391
Gean Ferreira <gean_roberto@hotmail.com>
2022-08-03 21:28:42 -0300

    FEAT: Adicionando nova funcionalidade flip 3d
    Neste commit foi adicionado uma nova funcionalidade que permite ver duas imagens do mesmo produto devido a animacao 3d. Quando ocorrer evento hover a imagem que esta escondida por detras da imagem frontal aparece, e quanto tira, a imagem frotal reaparece novamente. Foi colocado uma imagem com opacidade 0 para determinar o tamanho das imagens com o position absolute. Para ocorrer a animacao corretamente criou-se uma div pai de ambas as imagens, por isso, a div que denominava-se product__container foi excluida

--- src/components/CardProduct/index.js
+++ src/components/CardProduct/index.js
@@ -1,18 +1,25 @@
 import './style.css';
 import { convertToReal } from '../../assets/scripts/functions';
 
-export const CardProduct = ({ link, image, name, price }) => {
-  return (
-    <div className='product__container'>
-      <a className='link__product' href={link} target='_self' rel='noopener noreferrer'>
-        <img src={image} alt={`Fotos de ${name}`} width={'100%'} height={'auto'} />
-        <h5 className='title__product'>{name}</h5>
-        <p className='price__product'>{convertToReal(price)}</p>
-      </a>
-    </div>
+export const CardProduct = ({ link, image1, image2, name, price }) => {
+    return (
+        <a className='link__product' href={link} target='_self' rel='noopener noreferrer'>
+            {/* Container pai das imagens da animação 3D */}
+            <div className='flip-inner'>
+                <div className='flip-front'>
+                    <img className='image__product' src={image1} alt={`Fotos de ${name}`} width={'100%'} height={'auto'} />
+                </div>
+                <div className='flip-back'>
+                    <img className='image__product' src={image2} alt={`Fotos de ${name}`} width={'100%'} height={'auto'} />
+                </div>
+            </div>
+            {/* Imagem para dimensionar os flips */}
+            <img className='image__product' src={image1} alt={`Fotos de ${name}`} width={'100%'} height={'auto'} style={{ opacity: '0' }} />
+            {/* Detalhes do produto */}
             <div className='details__product'>
                 <h5 className='title__details'>{name}</h5>
                 <span className='price__details'>{convertToReal(price)}</span>
             </div>
-  );
+        </a>
+    );
 };
