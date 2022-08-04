3c89226bb1519edbf7fa9b67dd42f926c88710b9
Gean Ferreira <gean_roberto@hotmail.com>
2022-08-03 21:45:12 -0300

    FEAT: Listagem dos produtos sessao ProductsSection
    Funcao que retorna a lista dos produtos. Essa listagem chama o component CardProduct no qual faz a estilizacao dos dados inputados como propriedades

--- src/components/ProductsSection/index.js
+++ src/components/ProductsSection/index.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import './style.css';
+import { CardProduct } from '../CardProduct';
 
 export const Products = () => {
   // Constante que armazenará todos os produtos
@@ -9,7 +10,7 @@ export const Products = () => {
   const urlProducts = 'https://cantao.vtexcommercestable.com.br/api/catalog_system/pub/products/search?fC:65&_from=1&_to=50';
 
   // Função que faz a requisição dos produtos
-    const getAnswer = async () => {
+  const getAnswer = async () => {
     await axios({
       url: urlProducts,
       method: 'GET',
@@ -17,33 +18,31 @@ export const Products = () => {
       setProducts(response.data);
     });
   };
-        
+
   // Ciclo de vida do React
   // Executa esta função toda vez que o componente for carregado
   useEffect(() => {
     getAnswer();
   }, []);
 
-        // Com fetch
-        // const resp = await fetch(urlProducts)
-        // const json = await resp.json()
-        // console.log(json)
-    }
+  // Função lista cada produto
+  function itemsMiddle() {
+    return products.map((response, index) => {
+      const { productName, link } = response;
+      const [img1, img2] = response.items[0].images;
+      const { Price } = response.items[0].sellers[0].commertialOffer;
+      return (
+        <li key={index}>
+          <CardProduct link={link} image1={img1.imageUrl} image2={img2.imageUrl} name={productName} price={Price} />
+        </li>
+      );
+    });
+  }
 
-    useEffect(() => {
-        getAnswer()
-    }, [])
-
-
-    return (
-        <main id="products" className="products__area container space">
-            <h1 className='title'>Produtos</h1>
-            <ul className="products__menu-list">
-                <li>produto</li>
-                <li>produto</li>
-                <li>produto</li>
-                <li>produto</li>
-            </ul>
-        </main>
-    )
-}
+  return (
+    <main id='products' className='products__area container space'>
+      <h1 className='title'>Produtos</h1>
+      <ul className='products__menu-list px-1'>{itemsMiddle()}</ul>
+    </main>
+  );
+};
