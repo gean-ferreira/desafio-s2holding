2ec29c2f7803d5024910c2a4ee037b93de124538
Gean Ferreira <gean_roberto@hotmail.com>
2022-08-03 22:53:56 -0300

    FEAT: Nova funcionalidade em MiddleSection
    A nova funcionalidade consiste em observar se a tela do usuario ira diminuir ou aumentar. O evento possui uma callback que determina o valor da variavel de estado show com a funcao isMobile, assim, sempre deixando atualizado o render da sessao

--- src/components/Middle/index.js
+++ src/components/Middle/index.js
@@ -5,71 +5,77 @@ import img3 from '../../assets/imgs/middle/middle-3.jpg';
 import img4 from '../../assets/imgs/middle/middle-4.jpg';
 import { isMobile } from '../../assets/scripts/functions';
 import Carousel from 'nuka-carousel';
+import { useState } from 'react';
 
 export const Middle = () => {
-  // Array com o caminho relativo das imagens
-  const imgsMiddle = [img1, img2, img3, img4];
+    // Array com o caminho relativo das imagens
+    const imgsMiddle = [img1, img2, img3, img4];
+    const [show, setShow] = useState(isMobile());
 
-  // Condicional que adiciona classe global para manipular o estilo do item
-  const inTheLeft = index => (index % 2 ? '' : ' justify-content-end');
+    // Condicional que adiciona classe global para manipular o estilo do item
+    const inTheLeft = index => (index % 2 ? '' : ' justify-content-end');
 
-  // Lista os itens da sessão Middle
-  function itemsMiddle() {
-    return imgsMiddle.map((res, index) => {
-      return (
-        <li key={index} className={`middle__items${inTheLeft(index)}`}>
-          <div className='image__middle'>
-            <a href='http://cantao.com.br' target='_blank' rel='noopener noreferrer'>
-              <img width={'100%'} src={res} alt={`Imagem do meio ${index}`} />
-              <div className='banner-bottom__image'>
-                <span>Confira nossa categoria {index + 1}!</span>
-              </div>
-            </a>
-          </div>
-        </li>
-      );
+    // Lista os itens da sessão Middle
+    function itemsMiddle() {
+        return imgsMiddle.map((res, index) => {
+            return (
+                <li key={index} className={`middle__items${inTheLeft(index)}`}>
+                    <div className='image__middle'>
+                        <a href='http://cantao.com.br' target='_blank' rel='noopener noreferrer'>
+                            <img width={'100%'} src={res} alt={`Imagem do meio ${index}`} />
+                            <div className='banner-bottom__image'>
+                                <span>Confira nossa categoria {index + 1}!</span>
+                            </div>
+                        </a>
+                    </div>
+                </li>
+            );
+        });
+    }
+
+    // Estilização dos botões do Carousel
+    const stylebuttons = {
+        default: { fontSize: '35px', fontWeight: 'bold' },
+        rightButton: function () {
+            return { ...this.default, borderRadius: '50% 0 0 50%', padding: '10px 5px 10px 15px' };
+        },
+        leftButton: function () {
+            return { ...this.default, borderRadius: '0 50% 50% 0', padding: '10px 15px  10px 5px ' };
+        },
+    };
+
+    // Mobile: a função renderiza as imagens dentro de uma lib de Carousel
+    // Desktop: renderiza a sessão em duas colunas
+    function renderSection() {
+        return show ? (
+            <Carousel
+                autoplay={true}
+                dragThreshold={0.1}
+                wrapAround={true}
+                defaultControlsConfig={{
+                    pagingDotsStyle: { display: 'none' },
+                    nextButtonStyle: stylebuttons.rightButton(),
+                    nextButtonText: '>',
+                    prevButtonStyle: stylebuttons.leftButton(),
+                    prevButtonText: '<',
+                }}
+            >
+                {itemsMiddle()}
+            </Carousel>
+        ) : (
+            <ul className='middle__menu-list px-1'>{itemsMiddle()}</ul>
+        );
+    }
+
+    // Função que observa se o usuário irá diminuir a tela de tamanho
+    window.addEventListener('resize', () => {
+        setShow(isMobile());
     });
-  }
 
-  // Estilização dos botões do Carousel
-  const stylebuttons = {
-    default: { fontSize: '35px', fontWeight: 'bold' },
-    rightButton: function () {
-      return { ...this.default, borderRadius: '50% 0 0 50%', padding: '10px 5px 10px 15px' };
-    },
-    leftButton: function () {
-      return { ...this.default, borderRadius: '0 50% 50% 0', padding: '10px 15px  10px 5px ' };
-    },
-  };
-
-  // Função que observa se o usuário está no mobile
-  // Mobile: a função renderiza as imagens dentro de uma lib de Carousel
-  // Desktop: renderiza a sessão em duas colunas
-  function renderSection() {
-    return isMobile() ? (
-      <Carousel
-        autoplay={true}
-        dragThreshold={0.1}
-        wrapAround={true}
-        defaultControlsConfig={{
-          pagingDotsStyle: { display: 'none' },
-          nextButtonStyle: stylebuttons.rightButton(),
-          nextButtonText: '>',
-          prevButtonStyle: stylebuttons.leftButton(),
-          prevButtonText: '<',
-        }}
-      >
-        {itemsMiddle()}
-      </Carousel>
-    ) : (
-      <ul className='middle__menu-list px-1'>{itemsMiddle()}</ul>
+    return (
+        <section id='middle' className='middle__area container space'>
+            <h2 className='title'>Middle</h2>
+            {renderSection()}
+        </section>
     );
-  }
-
-  return (
-    <section id='middle' className='middle__area container space'>
-      <h2 className='title'>Middle</h2>
-      {renderSection()}
-    </section>
-  );
 };
