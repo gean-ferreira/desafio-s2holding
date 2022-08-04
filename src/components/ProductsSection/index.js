e9accc4e3dbeb138ff43e4f0ec683eaf09dd49b3
Gean Ferreira <gean_roberto@hotmail.com>
2022-08-03 23:54:20 -0300

    FEAT: Adicionando nova funcionalidade em ProductsSection
    A mesma logica do MiddleSection foi implementado nesta sessao. No mobile a sessao apresentara seus produtos num carousel e, a partir dos 768px, mortrar 3 produtos por linha e apos 1200px mostrara 4 produtos por linha. No mobile a estilizacao do titulo e o preco foram alteradas

--- src/components/ProductsSection/index.js
+++ src/components/ProductsSection/index.js
@@ -2,47 +2,90 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import './style.css';
 import { CardProduct } from '../CardProduct';
+import Carousel from 'nuka-carousel';
+import { isMobile } from '../../assets/scripts/functions';
 
 export const Products = () => {
-  // Constante que armazenará todos os produtos
-  const [products, setProducts] = useState([]);
-  // Url fornecida pelo desafio
-  const urlProducts = 'https://cantao.vtexcommercestable.com.br/api/catalog_system/pub/products/search?fC:65&_from=1&_to=50';
+    // Constante que armazenará todos os produtos
+    const [products, setProducts] = useState([]);
+    const [show, setShow] = useState(isMobile());
+    // Url fornecida pelo desafio
+    const urlProducts = 'https://cantao.vtexcommercestable.com.br/api/catalog_system/pub/products/search?fC:65&_from=1&_to=50';
 
-  // Função que faz a requisição dos produtos
-  const getAnswer = async () => {
-    await axios({
-      url: urlProducts,
-      method: 'GET',
-    }).then(response => {
-      setProducts(response.data);
+    // Função que faz a requisição dos produtos
+    const getAnswer = async () => {
+        await axios({
+            url: urlProducts,
+            method: 'GET',
+        }).then(response => {
+            setProducts(response.data);
+        });
+    };
+
+    // Ciclo de vida do React
+    // Executa esta função toda vez que o componente for carregado
+    useEffect(() => {
+        getAnswer();
+    }, []);
+
+    // Estilização dos botões do Carousel
+    const stylebuttons = {
+        default: { fontSize: '35px', fontWeight: 'bold' },
+        rightButton: function () {
+            return { ...this.default, borderRadius: '50% 0 0 50%', padding: '10px 5px 10px 15px' };
+        },
+        leftButton: function () {
+            return { ...this.default, borderRadius: '0 50% 50% 0', padding: '10px 15px  10px 5px ' };
+        },
+    };
+
+    // Função lista cada produto
+    function productList() {
+        return products.map((response, index) => {
+            const { productName, link } = response;
+            const [img1, img2] = response.items[0].images;
+            const { Price } = response.items[0].sellers[0].commertialOffer;
+            return (
+                <li key={index}>
+                    <CardProduct link={link} image1={img1.imageUrl} image2={img2.imageUrl} name={productName} price={Price} />
+                </li>
+            );
+        });
+    }
+
+    // Mobile: a função renderiza as imagens dentro de uma lib de Carousel
+    // iPad: renderiza 3 produtos por linha
+    // Desktop: renderiza 4 produtos por linha
+    function renderSection() {
+        return show ? (
+            <Carousel
+                autoplay={true}
+                dragThreshold={0.1}
+                wrapAround={true}
+                defaultControlsConfig={{
+                    pagingDotsStyle: { display: 'none' },
+                    nextButtonStyle: stylebuttons.rightButton(),
+                    nextButtonText: '>',
+                    prevButtonStyle: stylebuttons.leftButton(),
+                    prevButtonText: '<',
+                }}
+            >
+                {productList()}
+            </Carousel>
+        ) : (
+            <ul className='products__menu-list px-1'>{productList()}</ul>
+        );
+    }
+
+    // Função que observa se o usuário irá diminuir a tela de tamanho
+    window.addEventListener('resize', () => {
+        setShow(isMobile());
     });
-  };
 
-  // Ciclo de vida do React
-  // Executa esta função toda vez que o componente for carregado
-  useEffect(() => {
-    getAnswer();
-  }, []);
-
-  // Função lista cada produto
-  function itemsMiddle() {
-    return products.map((response, index) => {
-      const { productName, link } = response;
-      const [img1, img2] = response.items[0].images;
-      const { Price } = response.items[0].sellers[0].commertialOffer;
-      return (
-        <li key={index}>
-          <CardProduct link={link} image1={img1.imageUrl} image2={img2.imageUrl} name={productName} price={Price} />
-        </li>
-      );
-    });
-  }
-
-  return (
-    <main id='products' className='products__area container space'>
-      <h1 className='title'>Produtos</h1>
-      <ul className='products__menu-list px-1'>{itemsMiddle()}</ul>
-    </main>
-  );
+    return (
+        <main id='products' className='products__area container space'>
+            <h1 className='title'>Produtos</h1>
+            {renderSection()}
+        </main>
+    );
 };
