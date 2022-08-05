5e567744ce4fb3333b1ed466401de90bb58f035c
Gean Ferreira <gean_roberto@hotmail.com>
2022-08-04 21:14:33 -0300

    FEAT: Nova component Loading
    Esse component tera uma funcao importantissima no site. Enquanto o consumo da API é realizada na pagina o loading aparecera na tela melhorando a ux do usuario

---
+++ src/components/Loading/index.js
@@ -0,0 +1,11 @@
+import './style.css';
+
+export const Loading = () => {
+    return (
+        <div class='snippet' data-title='.dot-pulse'>
+            <div class='stage'>
+                <div class='dot-pulse'></div>
+            </div>
+        </div>
+    );
+};
