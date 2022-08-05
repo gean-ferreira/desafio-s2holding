af8656736ce1ab8b4f6227dc90afaeb8da165ef8
Gean Ferreira <gean_roberto@hotmail.com>
2022-08-04 22:28:07 -0300

    WARNING: Resolve warning do component Image
    O warning alegava que a variavel loading nao estava sendo utilizada. A variavel so estava sendo utilizada para mudanca de estado, entao, optou-se por utilizar o hook useRef

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -1,16 +1,16 @@
-import { useState } from 'react';
+import { useRef } from 'react';
 import './style.css';
 
 export const Image = ({ classNameImg, src, alt, height, width }) => {
-    const [loading, setLoading] = useState(true);
+    const loading = useRef(true);
 
     // Esta função é apenas chamada quando a imagem é carregada
     function imgLoad() {
-        setLoading(false);
+        loading.current(false);
     }
 
     return (
-        <div onLoad={imgLoad} style={loading ? { height: parentHeight } : { height: 'auto' }}>
+        <div onLoad={imgLoad}>
             <img height={height} width={width} className={`image-component${classNameImg ? ` ${classNameImg}` : ''}`} src={src} alt={alt} />
         </div>
     );
