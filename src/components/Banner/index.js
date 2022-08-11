a85a4a80df14cc551edecbd5984c173895f89aa5
Gean Ferreira <gean_roberto@hotmail.com>
2022-08-10 22:49:54 -0300

    REFACTOR: Rafatora o component Banner
    Neste commit altera a mudanca das imagens por meio do srcset que é uma proprieda da tag html ao inves de alterar o background da sessao atraves de media querie do css. Foi importado tambem o hook para determinar o tamanho mínimo das imagens de acordo com o viewport que o usuario carregar a aplicacao

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,10 +1,20 @@
-import './style.css';
+import imgMob from '../../assets/imgs/banners/banner-full-mobile.jpg';
+import imgDesk from '../../assets/imgs/banners/banner-full-desktop.jpg';
+import { useViewPort } from '../../assets/scripts/hooks/useViewPort';
 
 export const Banner = () => {
+    const { isMobile } = useViewPort();
+
     return (
-        <a href="https://cantao.com.br" target="_self" rel="noopener noreferrer">
-            <section id="banner">
-            </section>
-        </a>
-    )
-}
+        <section id='banner'>
+            <a href='https://cantao.com.br' target='_self' rel='noopener noreferrer'>
+                <img
+                    srcSet={`${imgMob} 767w, ${imgDesk} 768w`}
+                    alt={'banner'}
+                    width={'100%'}
+                    style={{ minHeight: isMobile ? '620px' : '665px' }}
+                />
+            </a>
+        </section>
+    );
+};
